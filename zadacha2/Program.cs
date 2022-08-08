d900711e58cbd3f45563218fa209bb00be2e3c57
OlgaT <oliunea-t2002@mail.ru>
2022-08-08 21:37:36 +0300

    add zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу, которая на вход принимает два числа и проверяет, является ли 
+// перввое число квадратом второго
+Console.WriteLine("Ведите первое число");
+string numberString = Console.ReadLine();
+int numberA = int.Parse(numberString);
+Console.WriteLine("Введите второе число");
+string numberStringA = Console.ReadLine();
+int numberB = int.Parse(numberStringA);
+int sqrt = numberB * numberB;
+if (numberA == sqrt)
+{
+    Console.WriteLine("Да");
+} 
+else
+{
+    Console.WriteLine("Нет");
+}
