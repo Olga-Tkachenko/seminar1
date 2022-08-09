133a6e724a3430c9b150816491204b428751fef4
OlgaT <oliunea-t2002@mail.ru>
2022-08-09 13:04:34 +0300

    add zadacha 4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в
+// промежутке от -N до N. 
+Console.WriteLine("Введите число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+if (number<0)
+number = number*(-1);
+int i = number*(-1);
+while (i<=number)
+{
+    Console.WriteLine(i++);
+}
