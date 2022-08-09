aaaf381ecc9bc22a3850cd285183e1f7289a6054
OlgaT <oliunea-t2002@mail.ru>
2022-08-09 14:36:12 +0300

    add zadacha 5

---
+++ zadacha5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю 
+// цифру этого числа 
+Console.WriteLine("Введите трехзначное число");
+string? numberString1 = Console.ReadLine();
+int number1 = int.Parse(numberString1!);
+int number2 = number1-((number1/10)*10);
+if (number1<=999)
+if (number1>=100)
+Console.WriteLine("Последняя цифра введенного числа " + number2);
+else
+{
+    Console.WriteLine("Неверное число");
+}
